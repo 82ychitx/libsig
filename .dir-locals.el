31ae16a1e8f12090e08880c30501e7e0220e0b4e
Vojtech Tichy <xtichy28@vutbr.cz>
2026-03-12 20:32:11 +0100

    Loading file and getting double vals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,3 @@
-((nil . ((projectile-project-configure-cmd . "cmake -B build -G Ninja")
+((nil . ((projectile-project-configure-cmd . "cmake -B build -G Ninja -D CMAKE_BUILD_TYPE=Debug")
          (projectile-project-compilation-cmd . "cmake --build build")
          (projectile-project-run-cmd . "./build/libsig_bench")))) ; Replace "" with "./build/your_executable" later
--- app/main.c
+++ app/main.c
@@ -1,8 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "libsig.h"
+
+// TODO: Change this to dynamically change when we run out
+#define BUF_SIZE 1000
 
 int
 main(int argc, char* argv[])
 {
-    test();
+    FILE* data_file = fopen("./data/input/signal_small.csv", "r");
+    if (!data_file)
+        return 1;
+
+    // Allocate buffer for now
+    double* p_data = malloc(sizeof(double) * BUF_SIZE);
+    if (!p_data)
+        return 2;
+
+    char buf[32];
+    double line_val;
+    size_t data_num = 0;
+    
+    while(fgets(buf, sizeof(buf), data_file) != NULL) {
+        line_val = atof(buf);
+        p_data[data_num] = line_val;
+        ++data_num;
+    }
+
+    for (size_t i = 0; i < data_num; ++i) {
+        printf("value: %zu = %lf\n", i, p_data[i]);
+    }
+
+    free(p_data);
+    fclose(data_file);
     return 0;
 }

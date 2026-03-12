3df1ede6e3ecc1a74678058339dabd824a20de30
Vojtech Tichy <xtichy28@vutbr.cz>
2026-03-12 20:39:14 +0100

    Measuring time it takes to read data

--- app/main.c
+++ app/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "libsig.h"
 
@@ -9,28 +10,35 @@
 int
 main(int argc, char* argv[])
 {
-    FILE* data_file = fopen("./data/input/signal_small.csv", "r");
+    FILE* data_file;
+    clock_t tic;
+    char buf[32];
+    double line_val;
+    double* p_data;
+    size_t num_of_data = 0;
+    
+    data_file = fopen("./data/input/signal_small.csv", "r");
     if (!data_file)
         return 1;
 
     // Allocate buffer for now
-    double* p_data = malloc(sizeof(double) * BUF_SIZE);
+    p_data = malloc(sizeof(double) * BUF_SIZE);
     if (!p_data)
         return 2;
 
-    char buf[32];
-    double line_val;
-    size_t data_num = 0;
-    
+    tic = clock();
     while(fgets(buf, sizeof(buf), data_file) != NULL) {
         line_val = atof(buf);
-        p_data[data_num] = line_val;
-        ++data_num;
+        p_data[num_of_data] = line_val;
+        ++num_of_data;
     }
+    tic = clock() - tic;
 
-    for (size_t i = 0; i < data_num; ++i) {
+    for (size_t i = 0; i < num_of_data; ++i) {
         printf("value: %zu = %lf\n", i, p_data[i]);
     }
+
+    printf("\nTime it took reading the file: %lf", ((double)tic / CLOCKS_PER_SEC));
 
     free(p_data);
     fclose(data_file);
